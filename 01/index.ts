01db2a0be8fb9bf907365b52d5389488bda28237
Oliver Johnston <oliver@ojohnston.com>
2021-12-01 21:52:36 +0000

    shorten

--- 01/index.ts
+++ 01/index.ts
@@ -1,20 +1,10 @@
 import * as utils from '../utils'
 import * as _ from 'lodash'
 
-function window(data: number[], windowSize: number) : number[][] {
-    let result = []
-    for(let i = 0; i <= data.length - windowSize; i++)
-    {
-        result.push(_.slice(data, i, i + windowSize))
-    }
-    return result
-}
-
 function solve(data: number[], windowSize: number) : number {
-    let values = window(data, windowSize).map(x => _.sum(x))
-    return values.filter((val, idx) => idx > 0 && val > values[idx-1]).length
+    let windowedValues = _.range(0, data.length - windowSize).map(idx => _.slice(data, idx, idx+windowSize)).map(x => _.sum(x))
+    return windowedValues.filter((val, idx) => idx > 0 && val > windowedValues[idx-1]).length
 }
-
 
 console.log(`Part 1 Example: ${solve(utils.getExampleLines(1, 1).map(x => parseInt(x)), 1)}`)
 console.log(`Part 1 Answer: ${solve(utils.getDataLines(1).map(x => parseInt(x)), 1)}`)
