b5028e7eee1712b2fc29eeb9dad396132b921733
Oliver Johnston <oliver@ojohnston.com>
2021-12-02 21:46:09 +0000

    2

--- 01/index.ts
+++ 01/index.ts
@@ -8,29 +8,11 @@ function solve(data: number[], windowSize: number): number {
     return windowedValues.filter((val, idx) => idx > 0 && val > windowedValues[idx - 1]).length
 }
 
-console.log(
-    'Part 1 Example: ',
-    solve(
-        utils.getExampleLines(1, 1).map((x) => parseInt(x)),
-        1
-    )
-)
-console.log(
-    `Part 1 Answer: ${solve(
-        utils.getDataLines(1).map((x) => parseInt(x)),
-        1
-    )}`
-)
+let exampleLines = utils.getExampleLines(1, 1).map((x) => parseInt(x))
+let dataLines = utils.getDataLines(1).map((x) => parseInt(x))
 
-console.log(
-    `Part 2 Example: ${solve(
-        utils.getExampleLines(1, 1).map((x) => parseInt(x)),
-        3
-    )}`
-)
-console.log(
-    `Part 2 Answer: ${solve(
-        utils.getDataLines(1).map((x) => parseInt(x)),
-        3
-    )}`
-)
+console.log(`Part 1 Example: ${solve(exampleLines, 1)}`)
+console.log(`Part 1 Answer: ${solve(dataLines, 1)}`)
+
+console.log(`Part 2 Example: ${solve(exampleLines, 3)}`)
+console.log(`Part 2 Answer: ${solve(dataLines, 3)}`)
---
+++ 02/index.ts
@@ -0,0 +1,51 @@
+import * as utils from '../utils'
+
+type Direction = 'forward' | 'down' | 'up'
+interface Command {
+    direction: Direction
+    units: number
+}
+
+function parseLine(line: string): Command {
+    let split = line.split(' ')
+    return {
+        direction: split[0] as Direction,
+        units: parseInt(split[1]),
+    }
+}
+
+function solve(commands: Command[]): number {
+    let x = 0,
+        y = 0
+
+    for (let command of commands) {
+        command.direction === 'forward' && (x += command.units)
+        command.direction === 'down' && (y += command.units)
+        command.direction === 'up' && (y -= command.units)
+    }
+
+    return x * y
+}
+
+function solve2(commands: Command[]): number {
+    let x = 0,
+        y = 0,
+        aim = 0
+
+    for (let command of commands) {
+        command.direction === 'forward' && (x += command.units) && (y += command.units * aim)
+        command.direction === 'down' && (aim += command.units)
+        command.direction === 'up' && (aim -= command.units)
+    }
+
+    return x * y
+}
+
+let exampleLines = utils.getExampleLines(2, 1).map((x) => parseLine(x))
+let dataLines = utils.getDataLines(2).map((x) => parseLine(x))
+
+console.log(`Part 1 Example: ${solve(exampleLines)}`)
+console.log(`Part 1 Answer: ${solve(dataLines)}`)
+
+console.log(`Part 2 Example: ${solve2(exampleLines)}`)
+console.log(`Part 2 Answer: ${solve2(dataLines)}`)
